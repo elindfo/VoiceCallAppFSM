5edf204a0ca0210144c72a6319ba4404374fc2fd
elindfo <elindfo@kth.se>
2018-09-25 19:15:12 +0200

    Add TODOs

--- Main.java
+++ Main.java
@@ -3,12 +3,15 @@ import state.VoiceAppStateHandler;
 
 import java.util.Scanner;
 
-//TODO Fråga Johnny om vi får använda koden från Caller-klassen
+//TODO Fråga om vi får använda koden från Caller-klassen
 //TODO Fråga om vi ska ha en eller flera inSession-klasser och om samtalsinitieringen är lika för båda sidor
+//TODO Fråga om det kanske räcker med en Ringing-klass istället för Ringing och Calling
+//TODO Fråga om enum för inkommande kommandon
+
 public class Main {
 
     private static final String MENU =
-            "1 - Invite\n" +
+                    "1 - Invite\n" +
                     "2 - Ack\n" +
                     "3 - Bye\n" +
                     "4 - Call\n" +
