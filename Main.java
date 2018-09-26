1db1b9f70130fd2c2b96181d4b83ee32db2601e1
elindfo <elindfo@kth.se>
2018-09-26 09:39:07 +0200

    Update TODOs

--- Main.java
+++ Main.java
@@ -6,23 +6,22 @@ import java.util.Scanner;
 //TODO Fråga om vi får använda koden från Caller-klassen - Svar ja!
 //TODO Fråga om enum för inkommande - Svar: Klass för översättning. Räkna med att UDP-port kan komma via invitet.
 
-//TODO Hantera errors i AbstractVoiceAppState (Utgå alltid från fel)
-//TODO isBusy()-metod som implementeras i AbstractBusyState
 //TODO Hantera BUSY utanför tillståndsmaskinen. Så fort någon kontaktar så fråga om maskinen är BUSY. Metod busy() som returnerar true/false som är overridad på olika sätt beroende på vilket tillstånd man är
 
-
-//Chronological order
-//TODO 1. Error handling in each state
-//TODO 2. Implement basic socket functionality to check if protocol works (no call establishing here)
+//Actual todos
+//TODO Create two different starting modes, one for receiving a call and one for initiating a call
+//TODO Implement basic socket functionality to check if protocol works (no call establishing here). Program menu for either answering call or initiating call needs to be made.
+//TODO Error handling over socket. Check so that the client disconnects if wrong protocol message is sent and that everything resets.
+//TODO Implement method for parsing messages received from socket
 
 public class Main {
 
     private static final String MENU =
-                    "1 - Invite\n" +
-                    "2 - Ack\n" +
-                    "3 - Bye\n" +
-                    "4 - Call\n" +
-                    "5 - Tro\n" +
+                            "1 - Invite\n" +
+                            "2 - Ack\n" +
+                            "3 - Bye\n" +
+                            "4 - Call\n" +
+                            "5 - Tro\n" +
                             "6 - EndCall\n" +
                             "7 - Ok\n" +
                             "8 - IsBusy";
